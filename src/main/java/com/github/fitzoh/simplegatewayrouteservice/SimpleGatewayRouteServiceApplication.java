6239870d4267887f0a89533537db8923e42c0f83
Fitzgerald, Andrew <andrewcfitzgerald@gmail.com>
2018-02-10 15:02:56 -0500

    initial commit from start.spring.io

---
+++ src/main/java/com/github/fitzoh/simplegatewayrouteservice/SimpleGatewayRouteServiceApplication.java
@@ -0,0 +1,12 @@
+package com.github.fitzoh.simplegatewayrouteservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleGatewayRouteServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleGatewayRouteServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/github/fitzoh/simplegatewayrouteservice/SimpleGatewayRouteServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.fitzoh.simplegatewayrouteservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SimpleGatewayRouteServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
