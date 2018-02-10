540970706c391f4696f7cbf5e215762ec8f3ab9f
Fitzgerald, Andrew <andrewcfitzgerald@gmail.com>
2018-02-10 15:26:57 -0500

    create route handle for route service requests

--- src/main/java/com/github/fitzoh/simplegatewayrouteservice/SimpleGatewayRouteServiceApplication.java
+++ src/main/java/com/github/fitzoh/simplegatewayrouteservice/SimpleGatewayRouteServiceApplication.java
@@ -2,11 +2,38 @@ package com.github.fitzoh.simplegatewayrouteservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SimpleGatewayRouteServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SimpleGatewayRouteServiceApplication.class, args);
-	}
+
+    public static final String FORWARDED_URL = "X-CF-Forwarded-Url";
+
+    public static final String PROXY_METADATA = "X-CF-Proxy-Metadata";
+
+    public static final String PROXY_SIGNATURE = "X-CF-Proxy-Signature";
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleGatewayRouteServiceApplication.class, args);
+    }
+
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r ->
+                        r.header(FORWARDED_URL, ".*")
+                                .and()
+                                .header(PROXY_METADATA, ".*")
+                                .and()
+                                .header(PROXY_SIGNATURE, ".*")
+                                .uri("http://google.com:80"))
+                .build();
+    }
+
+
 }
